b55e5a7063604a9f414d99d382bba490e2473b84
Jenn Perez <jennifer.perez@dir.texas.gov>
2023-06-26 16:42:50 -0500

    added on click active nav

--- activepage.js
+++ activepage.js
@@ -1,5 +1,12 @@
 const activePage=window.location.pathname;
-const navLinks=document.querySelectorAll('nav a').forEach(link=>{
-    console.log(link.href)
+const navLinkEls=document.querySelectorAll('a.links');
+const windowPathname=window.location.pathname;
+console.log(windowPathname)
+
+navLinkEls.forEach(navLinkEl=>{
+const navLinkPathname= new URL(navLinkEl.href).pathname;
+
+  if((windowPathname === navLinkPathname)|| (windowPathname==='/index.htm' && navLinkPathname==="/")){
+    navLinkEl.classList.add('active');
+  }
 })
-console.log(activePage);
